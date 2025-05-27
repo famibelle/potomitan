89c7b452906e4149f097a26fe5ccab3a329ecc10
Medhi Famibelle <medhi.famibelle@akabi.eu>
2025-05-27 21:39:02 +0200

    feedback button

--- src/components/AudioTranscriptionEditor.vue
+++ src/components/AudioTranscriptionEditor.vue
@@ -16,18 +16,19 @@
     <div v-for="file in visibleFiles" :key="file.id" class="row">
       <div class="column audio-col">
         <p class="filename">{{ file.name }}</p>
-          <div class="thumb-rating">
-            <span
-              class="thumb"
-              :class="{ selected: file.thumbVote === 'up' }"
-              @click="onThumbSelected(file, true)"
-            >👍</span>
-            <span
-              class="thumb"
-              :class="{ selected: file.thumbVote === 'down' }"
-              @click="onThumbSelected(file, false)"
-            >👎</span>
-          </div>        
+       <button aria-disabled="false" type="button" class="feedback-button" aria-label="Like" data-state="closed">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-thumbs-up">
+            <path d="M7 10v12"></path>
+            <path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2a3.13 3.13 0 0 1 3 3.88Z"></path>
+          </svg>
+       </button>
+
+        <button aria-disabled="false" type="button" class="feedback-button" aria-label="Dislike" data-state="closed">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-thumbs-down">
+            <path d="M17 14V2"></path>
+            <path d="M9 18.12 10 14H4.17a2 2 0 0 1-1.92-2.56l2.33-8A2 2 0 0 1 6.5 2H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.76a2 2 0 0 0-1.79 1.11L12 22a3.13 3.13 0 0 1-3-3.88Z"></path>
+          </svg>
+        </button>
         <audio
           :ref="el => audioRefs[file.id] = el"
           :src="file.url"
