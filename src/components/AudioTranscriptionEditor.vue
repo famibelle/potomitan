1100b67a0f58f6e561346b391e69bd06a59b17fc
Medhi Famibelle <medhi.famibelle@akabi.eu>
2025-06-03 22:46:42 +0200

    UI: ajout des messages de survol (title) sur les boutons de tri navigation rapide

--- src/components/AudioTranscriptionEditor.vue
+++ src/components/AudioTranscriptionEditor.vue
@@ -10,10 +10,10 @@
         <button @click="goToPrevious" :disabled="!hasPrevious" class="nav-btn">←</button>
         <span class="nav-status">Segment {{ currentIndexDisplay + 1 }} / {{ audioFiles.length }}</span>
         <button @click="goToNext" :disabled="!hasNext" class="nav-btn">→</button>
-        <button @click="shuffleFiles" class="nav-btn">🔀</button>
-        <button @click="sortByLikes" class="nav-btn">👍🏿</button>
-        <button @click="sortByDislikes" class="nav-btn">👎🏿</button>
-        <button @click="sortByStars" class="nav-btn">★</button>
+        <button @click="shuffleFiles" class="nav-btn" title="Mélanger l'ordre des fichiers">🔀</button>
+        <button @click="sortByLikes" class="nav-btn" title="Trier par nombre de likes">👍🏿</button>
+        <button @click="sortByDislikes" class="nav-btn" title="Trier par nombre de dislikes">👎🏿</button>
+        <button @click="sortByStars" class="nav-btn" title="Trier par nombre d'étoiles (cliquer à nouveau pour inverser)">★</button>
       </div>
     </div>
 
