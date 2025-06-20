54e51db1eb6d54096bad8f52929e8d262aec331a
Famibelle Médhi <medhi.famibelle@gmail.com>
2025-06-20 10:18:43 +0200

    Update db-init.sql

--- db-init.sql
+++ db-init.sql
@@ -1,6 +1,12 @@
-CREATE TABLE IF NOT EXISTS transcriptions (
-  id SERIAL PRIMARY KEY,
-  filename TEXT NOT NULL,
-  transcription TEXT NOT NULL,
-  timestamp TIMESTAMPTZ DEFAULT now()
+CREATE TABLE transcriptions (
+    id SERIAL PRIMARY KEY,
+    filename TEXT NOT NULL,
+    transcription TEXT NOT NULL,
+    rating SMALLINT DEFAULT 0,
+    timestamp TIMESTAMPTZ DEFAULT now(),
+    author TEXT DEFAULT 'whisper-large-v3',
+    likes INTEGER DEFAULT 0,
+    dislikes INTEGER DEFAULT 0,
+    created_at TIMESTAMP DEFAULT now(),
+    CONSTRAINT uq_filename_transcription UNIQUE (filename, transcription)
 );
